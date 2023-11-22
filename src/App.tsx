c8605ce7203b061b9632a93e2fb7f8f55888459f
Joao Lucas <joaolribeiro0811@gmail.com>
2023-11-22 15:54:35 -0300

    Project created and header ready

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import MainPage from "./routes/MainPage";
+
+export default function App() {
+
+  return (
+    <MainPage />
+  )
+}
---
+++ src/routes/MainPage/index.tsx
@@ -0,0 +1,8 @@
+import './styles.css';
+import Header from '../../components/Header';
+
+export default function MainPage() {
+    return (
+        <Header />
+    );
+}
