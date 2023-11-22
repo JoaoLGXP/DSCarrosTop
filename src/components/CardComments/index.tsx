b078200dd23ca0c3b63655da1140a617c134ee1b
Joao Lucas <joaolribeiro0811@gmail.com>
2023-11-22 17:57:17 -0300

    Third component finished

---
+++ src/components/CardComments/index.tsx
@@ -0,0 +1,9 @@
+import './styles.css';
+
+export default function CardComments() {
+    return (
+        <div className='dsct-comments-card'>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae harum voluptatem ad fugit id distinctio.</p>
+        </div>
+    );
+}
--- src/routes/MainPage/index.tsx
+++ src/routes/MainPage/index.tsx
@@ -1,6 +1,7 @@
 import './styles.css';
 import Header from '../../components/Header';
 import CardProduct from '../../components/CardProduct';
+import CardComments from '../../components/CardComments';
 
 export default function MainPage() {
     return (
@@ -10,7 +11,7 @@ export default function MainPage() {
                 <section id='dsct-section' className='dsc-bg-catalog'>
                     <div className='dsct-catalog'>
                         <h3 className='dsct-mb25'>Venha nos visitar</h3>
-                        <div className='dsct-products-cards'>
+                        <div className='dsct-cards-container'>
                             <CardProduct />
                             <CardProduct />
                         </div>
@@ -19,6 +20,12 @@ export default function MainPage() {
                 <section id='dsct-section' className='dsc-bg-comments'>
                     <div className='dsct-catalog'>
                         <h3 className='dsct-mb25'>O que estão dizendo</h3>
+                        <div className='dsct-cards-container'>
+                            <CardComments />
+                            <CardComments />
+                            <CardComments />
+                            <CardComments />
+                        </div>
                     </div>
                 </section>
             </main>
